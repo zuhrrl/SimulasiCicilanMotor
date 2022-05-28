52b1c4b73d2e39783bf7acecbee72646fa289b32
Zuhrul Anam <zuhrrlanam@gmail.com>
2022-05-28 22:57:25 +0700

    update: change admob code

--- lib/constant/ads_code.dart
+++ lib/constant/ads_code.dart
@@ -1,2 +1,2 @@
-const admobBannerCode = "ca-app-pub-3940256099942544/6300978111";
-const admobInterstitialCode = "ca-app-pub-3940256099942544/1033173712";
+const admobBannerCode = "ca-app-pub-5144330013420313/2530879747";
+const admobInterstitialCode = "ca-app-pub-5144330013420313/4965471391";
